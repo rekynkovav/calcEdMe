2ead50002fc1b336fe176fa23ed142d80740e74c
rekynkovav <rekynkovav@users.noreply.github.com>
2024-12-20 10:08:22 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,13 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             System.out.println(calc(bufferedReader.readLine()));
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            System.out.println("Ошибка ввода данных");
         }
     }
 
@@ -19,24 +18,29 @@ public class Main {
         String[] arrayStr = input.split(" ");
         int x = Integer.parseInt(arrayStr[0]);
         int x2 = Integer.parseInt(arrayStr[2]);
+        String c = arrayStr[1];
         String result = "";
-        if (x < 0 || x > 10 && x2 < 0 || x2 > 11 && input.length() > 3) {
-           throw  new Exception();
-        }else{
-            switch ((int) arrayStr[1].charAt(0)) {
-                case 43:
-                    result = x + x2 + "";
-                    break;
-                case 42:
-                    result = x * x2 + "";
-                    break;
-                case 45:
-                    result = x - x2 + "";
-                    break;
-                case 47:
-                    result =  x / x2 + "";
-                    break;
-            }
+        if (x < 1 || x > 10 || x2 < 1 || x2 > 10) {
+            throw new Exception("Значения должны быть от 1 до 10 включительно");
+        }
+        if (input.length() > 3){
+            throw new Exception("Данные передаются в одну строку: a + b, a - b, a * b, a / b");
+        }
+        switch (c) {
+            case "+":
+                result = x + x2 + "";
+                break;
+            case "*":
+                result = x * x2 + "";
+                break;
+            case "-":
+                result = x - x2 + "";
+                break;
+            case "/":
+                result = x / x2 + "";
+                break;
+            default:
+                System.out.println("Вы ввели неверное значение");
         }
         return result;
     }
